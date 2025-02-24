16dc969340d2578a94853a435b9f228946dce534
unknown <trungsieunhan24@gmail.com>
2025-02-24 21:48:12 +0700

    add source base

---
+++ RoninBooking/RoninBooking/src/main/java/com/example/RoninBooking/RoninBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.RoninBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoninBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoninBookingApplication.class, args);
+	}
+
+}
---
+++ RoninBooking/RoninBooking/src/test/java/com/example/RoninBooking/RoninBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoninBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoninBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
